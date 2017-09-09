c34f299ae0564c6fdf8a803cf93db571b61908c5
Alexis Clavijo <asc477@nyu.edu>
2017-09-09 11:33:40 -0400

    first change

--- riemann.py
+++ riemann.py
@@ -3,13 +3,14 @@
 # The following is an example for how to define a function in Python
 # def tells the compiler that hello_world is the name of a function
 # this implementation of hello_world takes a string as an argument,
-# which has default value of the empty string. If the user calls 
+# which has default value of the empty string. If the user calls
 # hello_world() without an argument, then the compiler uses ''
 # as the default value of the argument.
 def hello_world(name=''):
     print "hello world!"
     print name
     return
-    
-    
+
+hello_world()
+
 #Implement the Riemann Sum approximation for integrals.
